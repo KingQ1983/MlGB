ec0297b5145495d423f74b36d66f1aee3ec9e9c9
KingQ1983 <79850699+KingQ1983@users.noreply.github.com>
2021-03-04 19:55:14 +0800

    Update Lucky_main.py

--- -/Lucky_main.py
+++ -/Lucky_main.py
@@ -87,7 +87,7 @@ def watch(flag,list):
 def readdata(id):
    enddata=[]
    try:
-     with open('./Lucky/Data'+str(id)+".txt", "r") as f:
+     with open('./-/Data'+str(id)+".txt", "r") as f:
        i=0
        for line in f.readlines():
         line = line.strip('\n')
