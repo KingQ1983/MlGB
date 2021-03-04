65d126b6493a9587948375e2aee3032fe8fea605
KingQ1983 <79850699+KingQ1983@users.noreply.github.com>
2021-03-04 19:33:06 +0800

    Update Lucky_main.py

--- -/Lucky_main.py
+++ -/Lucky_main.py
@@ -30,8 +30,7 @@ count={}
 osenviron['lucky_com_hd']='''
 {"Accept": "*/*","Accept-Encoding": "br, gzip, deflate","Accept-Language": "zh-Hans-CN;q=1, en-US;q=0.9, zh-Hant-CN;q=0.8","Content-Type":"application/x-www-form-urlencoded","User-Agent": "KDApp/1.8.2 (iPhone; iOS 12.4; Scale/2.00)",}
 '''
-osenviron['lucky_main_num']='''
-10
+osenviron['lucky_main_num']='''3
 '''
 def Av(i,hd,k,key=''):
    global count
