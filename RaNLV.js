e675168d2eef63d88825105ac96c7ee509378350
KingQ1983 <79850699+KingQ1983@users.noreply.github.com>
2021-03-05 14:42:15 +0800

    Update RaNLV.js

--- RaNLV.js
+++ RaNLV.js
@@ -36,8 +36,6 @@ let rlurl = $.getdata('rlurl')
 let rlheader = $.getdata('rlheader')
 let rlbody = $.getdata('rlbody')
 let tz = ($.getval('tz') || '1');//0关闭通知，1默认开启
-//let cash = ($.getval('rlcash') || '0')//默认不自动提现
-let cash='10';
 const invite=1;//新用户自动邀请，0关闭，1默认开启
 const logs =0;//0为关闭日志，1为开启
 var hour=''
@@ -56,38 +54,27 @@ if (isGetCookie) {
    $.done()
 } 
 if ($.isNode()) {
- 
-
- if (process.env.RLURL && process.env.RLURL.indexOf('#') > -1) {
-   rlurl = process.env.RLURL.split('#');
+   if (process.env.RLURL && process.env.RLURL.indexOf('#') > -1) {
+   rlurlArr = process.env.RLURL.split('#');
    console.log(`您选择的是用"#"隔开\n`)
   }
   else if (process.env.RLURL && process.env.RLURL.indexOf('\n') > -1) {
-   rlurl = process.env.RLURL.split('\n');
+   rlurlArr = process.env.RLURL.split('\n');
    console.log(`您选择的是用换行隔开\n`)
   } else {
-   rlurl = process.env.RLURL.split()
+   rlurlArr = process.env.RLURL.split()
   };
   if (process.env.RLHEADER && process.env.RLHEADER.indexOf('#') > -1) {
-   rlheader = process.env.RLHEADER.split('#');
+   rlheaderArr = process.env.RLHEADER.split('#');
    console.log(`您选择的是用"#"隔开\n`)
   }
   else if (process.env.RLHEADER && process.env.RLHEADER.indexOf('\n') > -1) {
-   rlheader = process.env.RLHEADER.split('\n');
+   rlheaderArr = process.env.RLHEADER.split('\n');
    console.log(`您选择的是用换行隔开\n`)
   } else {
-   rlheader = process.env.RLHEADER.split()
+   rlheaderArr = process.env.RLHEADER.split()
   };
-  if (process.env.RLBODY && process.env.RLBODY.indexOf('#') > -1) {
-   rlbody = process.env.RLBODY.split('#');
-   console.log(`您选择的是用"#"隔开\n`)
-  }
-  else if (process.env.RLBODY && process.env.RLBODY.indexOf('\n') > -1) {
-   rlbody = process.env.RLBODY.split('\n');
-   console.log(`您选择的是用换行隔开\n`)
-  } else {
-   rlbody = process.env.RLBODY.split()
-  };
+  
     console.log(`============ 脚本执行-国际标准时间(UTC)：${new Date().toLocaleString()}  =============\n`)
     console.log(`============ 脚本执行-北京时间(UTC+8)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}  =============\n`)
  } else {
@@ -101,19 +88,30 @@ if ($.isNode()) {
     rlbodyArr.push($.getdata(`rlbody${i}`))
   }
 }
+
+
 !(async () => {
-if (!rlheaderArr[0] && !rlurlArr[0]) {
+if (!rlheaderArr[0] && !rlbodyArr[0] && !rlurlArr[0]) {
     $.msg($.name, '【提示】请先获取燃旅视频一cookie')
     return;
   }
    console.log(`------------- 共${rlheaderArr.length}个账号----------------\n`)
-  for (let i = 0; i < rlheaderArr.length; i++) {
+  for (let i = 1; i < rlheaderArr.length; i++) {
     if (rlheaderArr[i]) {
       message = ''
       note =''
       rlurl = rlurlArr[i];
       rlheader = rlheaderArr[i];
-      //rlbody = rlbodyArr[i];
+      rlbody = rlbodyArr[i];
+
+console.log(rlurl)
+
+console.log(rlheader)
+
+
+//return
+
+
       $.index = i + 1;
       console.log(`\n开始【燃旅视频${$.index}】`)
       await checkVersion()
@@ -123,7 +121,6 @@ if (!rlheaderArr[0] && !rlurlArr[0]) {
       await myVotes()
       await wiTask()
       await showmsg()
-      
   }
  }
 })()
@@ -132,11 +129,11 @@ if (!rlheaderArr[0] && !rlurlArr[0]) {
     
 function GetCookie() {
 if($request&&$request.url.indexOf("Common/pvlog")>=0) {
-   const rlurl = $request.url.split('?')[1]
+   let rlurl = $request.url.split('?')[1]
    if(rlurl)     $.setdata(rlurl,`rlurl${status}`)
    $.log(`[${zhiyi}] 获取rlurl请求: 成功,rlurl: ${rlurl}`)
    $.msg(`rlurl${status}: 成功🎉`, ``)
-   const rlheader = JSON.stringify($request.headers)
+   let rlheader = JSON.stringify($request.headers)
     if(rlheader)    $.setdata(rlheader,`rlheader${status}`)
     $.log(`[${zhiyi}] 获取rlheader请求: 成功,rlheader: ${rlheader}`)
     $.msg(`rlheader${status}: 成功🎉`, ``)
@@ -218,9 +215,6 @@ let headers = rlheader.replace(/acw_tc=\w+/,'')
         myid = result.user.id
         console.log('🎈'+result.msg+' 邀请码：'+result.user.id+' 昵称：'+result.user.nickname+' 燃旅号：'+result.user.ranlvid +'\n')
         console.log('现有余额：'+result.user.balance + '提现额度：'+result.user.lines+'\n')
-        if(cash > 0 && Number(result.user.balance) >= cash && Number(result.user.lines) >= Number(result.user.balance)){
-        await wallet()
-        }
         message += '🎈'+result.msg+' 邀请码：'+result.user.id+' 昵称：'+result.user.nickname+' 燃旅号：'+result.user.ranlvid +'现有余额：'+result.user.balance + '提现额度：'+result.user.lines+'\n'
         }else{
         console.log('👀我也不知道\n')
@@ -761,76 +755,6 @@ let url = rlurl.replace(/&video_id=\d{5}/,``)
         }else{
         console.log('👀'+result.msg+'\n')
         //message += '👀'+"我也不知道\n"
-        }
-        }catch(e) {
-          $.logErr(e, response);
-      } finally {
-        resolve();
-      } 
-    })
-   })
-}
-//withdraw
-async function withdraw(){
-let url = rlurl.replace(/&video_id=\d{5}/,``)
- return new Promise((resolve) => {
-    let withdraw_url = {
-   		url: `https://ranlv.lvfacn.com/api.php/Share/withdraw?&amount=${cash}&is_act=1&member_id=${myid}&${url}`,
-    	headers: JSON.parse(rlheader),
-    	}
-   $.post(withdraw_url,async(error, response, data) =>{
-    try{
-        const result = JSON.parse(data)
-        if(logs) $.log(data)
-        if(result.code == 0){
-        console.log(`成功提现${cash}元\n`)
-        message += `成功提现${cash}元\n`
-        }else{
-        console.log('👀'+result.msg+'\n')
-        }
-        }catch(e) {
-          $.logErr(e, response);
-      } finally {
-        resolve();
-      } 
-    })
-   })
-}
-//wallet
-async function wallet(){
-let url = rlurl.replace(/&video_id=\d{5}/,``)
- return new Promise((resolve) => {
-    let wallet_url = {
-   		url: `https://ranlv.lvfacn.com/api.php/Share/wallet?&&list_rows=1&page=1&type=2&member_id=${myid}&${url}`,
-    	headers: JSON.parse(rlheader),
-    	}
-   $.post(wallet_url,async(error, response, data) =>{
-    try{
-        const result = JSON.parse(data)
-        if(logs) $.log(data)
-        if(result.code == 0){
-        let hour,minute,second,year,month,day;
-year = (new Date()).getFullYear();
-month = (new Date()).getMonth() + 1;
-day = (new Date()).getDate();
-if (month >= 1 && month <= 9) {
-            month = "0" + month;
-    }
-if (day >= 0 && day <= 9) {
-            day = "0" + day;
-   }
-hour = (new Date()).getHours();
-minute = (new Date()).getMinutes();
-second = (new Date()).getSeconds();
-let now = Number(year+month+day+hour+minute+second)
-let cashArr = result.data.data.data.find(item => item.description === '提现')
-let create_time = Number(cashArr.serialnum.match(/\d{14}/))
-if(now - create_time >= 1000000){
-$.log(`设置的提现金额为${cash},开始提现\n`)
-await withdraw()
-}
-        }else{
-        console.log('👀'+result.msg+'\n')
         }
         }catch(e) {
           $.logErr(e, response);
