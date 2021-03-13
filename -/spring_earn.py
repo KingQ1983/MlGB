fdac99419166c7e0a09eab796853c08c14d88766
KingQ1983 <79850699+KingQ1983@users.noreply.github.com>
2021-03-14 00:12:13 +0800

    Update spring_earn.py

--- -/spring_earn.py
+++ -/spring_earn.py
@@ -22,23 +22,34 @@ issign=0
 isdoubsign=0
 isnooncoin=0
 xmly_bark_cookie=''
+djj_sever_jiang=''
 djj_tele_cookie=''
 
 
 
+header={"Accept": "*/*","Accept-Encoding": "gzip, deflate","Accept-Language": "zh-Hans-CN;q=1, en-US;q=0.9, zh-Hant-CN;q=0.8","Connection": "close","Content-Type": "application/x-www-form-urlencoded","Host": "cf-api.douzhuanapi.cn:10002","User-Agent": "NormalDemo/1 (iPhone; iOS 14.4; Scale/2.00)","X-V": "1","osType": "iOS","phoneModel": "iPhone XR","platform": "iOS","versioncode": "1",}
 
-
-
-header={"Accept": "*/*","Accept-Encoding": "gzip, deflate","Accept-Language": "zh-Hans-CN;q=1, en-US;q=0.9, zh-Hant-CN;q=0.8","Connection": "close","Content-Type": "application/x-www-form-urlencoded","Host": "cf-api.douzhuanapi.cn:10002","User-Agent": "NormalDemo/1 (iPhone; iOS 14.4; Scale/2.00)","X-V": "1","osType": "iOS","phoneModel": "","platform": "iOS","versioncode": "1",}
-phoneModel=['iPhone XR','iPhone 7 Plus','iPhone 6s Plus','iPhone SE','iPad mini (WiFi)','iPhone 6 Plus','iPhone 5S','iPhone 8 Plus','iPhone 6','iPhone 6s Plus']
 
 
 def spring_earn():
    getuser()
    self_task_info()
-   getnooncoin()
+   
    getsigninfo()
    getsign()
+   time.sleep(2)
+   task_award(13)
+   time.sleep(1)
+   task_award(14)
+   time.sleep(1)
+   task_do(15,2)
+   task_award(15)
+   time.sleep(1)
+   task_award(16)
+   time.sleep(1)
+   task_do(5,5)
+   time.sleep(1)
+   task_award(5)
    treasure_box_gain()
    self_readlist()
    self_read()
@@ -50,9 +61,9 @@ def getuser():
      
      response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/user',headers=header)
      Res=response.json()
-     print(Res['message'])
+     print(Res)
      if Res['code']==200:
-       msg+=Res['data']['nick_name']+'|累收'+Res['data']['total_gain']+'|今收'+Res['data']['today_gain']+'|转收'+Res['data']['forward_gain']+'|现金余额'+Res['data']['balance']+'|'
+       msg+=Res['data']['nick_name']+'|累计收入'+Res['data']['total_gain']+'|今日收入'+Res['data']['today_gain']+'|转发收入'+Res['data']['forward_gain']+'|现金余额'+Res['data']['balance']+'|'
    except Exception as e:
       print(str(e))
    loger(msg)
@@ -83,7 +94,7 @@ def self_task_info():
            
        
    except Exception as e:
-      msg=str(e)
+      print(str(e))
    print(msg)
       
 
@@ -112,34 +123,28 @@ def getsigninfo(flag=0):
    loger(msg)
 
 
-def getnooncoin():
-   print('\n getnooncoin')
-   try:
-     msg=''
-     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/get_red_task_gold?id=16',headers=header)
-     Res=response.json()
-     print(Res)
-     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/get_red_task_gold?id=17',headers=header)
-     Res=response.json()
-     print(Res)
-     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/get_red_task_gold?id=18',headers=header)
-     Res=response.json()
-     print(Res)
-   except Exception as e:
-      print(str(e))
-      
-      
-      
+
 def treasure_box_gain():
    print('\n treasure_box_gain')
    try:
      msg=''
-     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/treasure_box_gain?treasure_box_id=425924&type=1',headers=header)
+     
+     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/treasure_box_opt',headers=header)
      Res=response.json()
      print(Res)
-     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/treasure_box_gain?gold_gain_id=12284065&treasure_box_id=425924&type=2',headers=header)
+     treasureBox_id=Res['data']['treasureBox_id']
+     time.sleep(1)
+     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/treasure_box_gain?treasure_box_id='+str(treasureBox_id)+'&type=1',headers=header)
      Res=response.json()
      print(Res)
+     gold_gain_id=Res['data']['gold_gain_id']
+     time.sleep(1)
+     
+     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/treasure_box_gain?gold_gain_id='+str(gold_gain_id)+'&treasure_box_id='+str(treasureBox_id)+'&type=2',headers=header)
+     Res=response.json()
+     print(Res)
+     
+     
    except Exception as e:
       print(str(e))
       
@@ -162,9 +167,33 @@ def getsign():
      Res=response.json()
      print(Res)
    except Exception as e:
-      print(str(e))
-      
+      msg=str(e)
+      print(msg)
 
+def task_award(id):
+   print('\n task_award'+str(id))
+   try:
+     msg=''
+     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/get_red_task_gold?id='+str(id),headers=header)
+     Res=response.json()
+     print(Res)
+     
+   except Exception as e:
+      msg=str(e)
+      print(msg)
+
+
+def task_do(item_id,task_type):
+   print('\n task_do'+str(item_id))
+   try:
+     msg=''
+     response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/red_task_report?item_id='+str(item_id)+'&task_type='+str(task_type),headers=header)
+     Res=response.json()
+     print(Res)
+     
+   except Exception as e:
+      msg=str(e)
+      print(msg)
 
 
 def self_readlist():
@@ -173,7 +202,7 @@ def self_readlist():
      global readidlist
      msg=''
      for i in range(19):
-       response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/article/list?city_type=1&page=4&slide=1&tag_id='+str(i)+'&type=1',headers=header)
+       response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/article/list?city_type=1&page=4&slide='+str(i)+'&tag_id='+str(i)+'&type=1',headers=header)
        Res=response.json()
      #print(Res)
        if Res['code']==200:
@@ -190,15 +219,20 @@ def self_readlist():
 def self_read():
    print('\n self_read')
    try:
-     msg='|'
+     msg='号码正常|'
      randlist=[]
      if len(readidlist)==0:
         return
-     for i in range(5):
+     for i in range(10):
         randlist.append(random.choice(readidlist))
      print(randlist)
      ii=0
      for readid in randlist:
+       ii+=1
+     if len(readidlist)==0:
+        return
+     ii=0
+     for readid in readidlist:
        ii+=1
        
        response = requests.get('http://cf-api.douzhuanapi.cn:10002/h5/article/article_detail?article_id='+str(readid),headers=header)
@@ -208,7 +242,7 @@ def self_read():
        else:
          print(f'''【{ii}】loading.....❌\n''')
          
-       bd='ad_source=1&location=3&position=5&report_type=1'
+       bd='ad_source=1&location=3&position=8&report_type=1'
        response = requests.post('http://cf-api.douzhuanapi.cn:10002/api/ad_sense/report',headers=header,data=bd)
        Res=response.json()
        print(Res)
@@ -231,7 +265,11 @@ def self_read():
          print(f'''【{ii}】awarding.....{Res['message']}\n''')
          if Res['message'].find('系统')>=0:
            msg+='号码变黑，明天再来'
-           break
+           bd='ad_source=1&location=3&position=8&report_type=1'
+           response = requests.post('http://cf-api.douzhuanapi.cn:10002/api/ad_sense/report',headers=header,data=bd)
+           Res=response.json()
+           print(Res)
+           #break
       
        response = requests.get('http://cf-api.douzhuanapi.cn:10002/api/self_read_init?item_id='+str(readid),headers=header)
        Res=response.json()
@@ -244,7 +282,7 @@ def self_read():
          msg+='本次完成阅读5篇'
          print('\n task competed=======')
          break
-       rm=random.randint(60,180)
+       rm=random.randint(60,80)
        print(f'''【{ii}】waiting.....{rm}s\n''')
        time.sleep(rm)
    except Exception as e:
@@ -258,11 +296,14 @@ def self_read():
 def watch(flag,list):
    vip=''
    global xmly_bark_cookie
+   global djj_sever_jiang
    global djj_tele_cookie
    if "XMLY_BARK_COOKIE" in os.environ:
       xmly_bark_cookie = os.environ["XMLY_BARK_COOKIE"]
    if "DJJ_TELE_COOKIE" in os.environ:
       djj_tele_cookie = os.environ["DJJ_TELE_COOKIE"]
+   if "DJJ_SEVER_JIANG" in os.environ:
+      djj_sever_jiang = os.environ["DJJ_SEVER_JIANG"]
    if flag in os.environ:
       vip = os.environ[flag]
    if flag in osenviron:
@@ -300,6 +341,15 @@ def pushmsg(title,txt,bflag=1,wflag=1,tflag=1):
          response = requests.get(turl,timeout=5)
    except Exception as e:
       print(str(e))
+   try:
+     if wflag==1 and djj_sever_jiang.strip():
+        print("\n【微信消息】")
+        purl = f'''http://sc.ftqq.com/{djj_sever_jiang}.send'''
+        headers={'Content-Type':'application/x-www-form-urlencoded; charset=UTF-8'}
+        body=f'''text={txt})&desp={title}'''
+        response = requests.post(purl,headers=headers,data=body)
+   except Exception as e:
+      print(str(e))
 def loger(m):
    #print(m)
    global result
@@ -323,17 +373,15 @@ def clock(func):
 def start():
    global result,bd
    print('Localtime',datetime.now(tz=tz.gettz('Asia/Shanghai')).strftime("%Y-%m-%d %H:%M:%S", ))
-   watch('SPRING_EARN_BODY',cklist)
+   watch('spring_earn_body',cklist)
    j=0
    for count in cklist:
      j+=1
      result+='【'+str(j)+'】'
      header['Authorization']=count
-     header['phoneModel']=phoneModel[j-1]
      spring_earn()
      result+='\n'
      #print(result)
-     time.sleep(300)
    pushmsg(Gamename,result)
 
 if __name__ == '__main__':
